ab184c2eacdff1b84dfde9d9e8adc37afdbda380
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-06-16 20:57:59 +0300

    commit tree 9d4d86568bb2d3c3ba5cdfd829389c3e5d2b440c

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const body = document.getElementById("main");
+const del = main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Amos June is the champion";
